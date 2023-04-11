d5e57429356cba5575e2ad6fba810def4e4c47af
DraculaKris <125392343+DraculaKris@users.noreply.github.com>
2023-04-11 09:48:04 -0300

    Delete lista5.py

--- lista5.py
+++
@@ -1,6 +0,0 @@
-liminferior = int(input("Digite o limite inferior: "))
-limsuperior = int(input("Digite o limite superior: "))
-incremento = int(input("Digite o incremento: "))
-
-for num in range(liminferior, limsuperior+1, incremento):
-    print(num)
